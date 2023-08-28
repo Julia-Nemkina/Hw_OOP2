fde87dd373916016d2dd41215e519abd8ace5a1c
Julia Nemkina <nemkina.julia@mail.ru>
2023-08-28 21:59:07 +0300

    generate class by lombok

--- src/main/java/Radio.java
+++ src/main/java/Radio.java
@@ -1,73 +1,13 @@
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class Radio {
     private int currentStation;
     private int currentVolume;
     private int maxStation;
 
-    public Radio() {
-        this.maxStation = 9;
-    }
-
-    public Radio(int stationsCount) {
-        this.maxStation = maxStation - 1;
-    }
-
-    public void next() {
-        if (currentStation != maxStation) {
-            currentStation++;
-        } else {
-            currentStation = 0;
-        }
-    }
-
-    public void prev() {
-        if (currentStation != 0) {
-            currentStation--;
-        } else {
-            currentStation = maxStation;
-        }
-    }
-
-    public void increaseVolume() {
-        if (currentVolume < 100) {
-            currentVolume++;
-        } else {
-            currentVolume = 100;
-        }
-    }
-
-    public void decreaseVolume() {
-        if (currentVolume != 0) {
-            currentVolume--;
-        } else {
-            currentVolume = 0;
-        }
-    }
-
-    public int getCurrentStation() {
-        return currentStation;
-    }
-
-    public void setCurrentStation(int currentStation) {
-        if (currentStation < 0) {
-            return;
-        }
-        if (currentStation > maxStation) {
-            return;
-        }
-        this.currentStation = currentStation;
-    }
-
-    public int getCurrentVolume() {
-        return currentVolume;
-    }
-
-    public void setCurrentVolume(int currentVolume) {
-        if (currentVolume < 0) {
-            return;
-        }
-        if (currentVolume > 100) {
-            return;
-        }
-        this.currentVolume = currentVolume;
-    }
 }
